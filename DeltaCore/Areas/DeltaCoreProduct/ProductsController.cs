736dc76df52011c8b0c6a0beff85d46222b8ebe3
Chaitali Mohite <77005224+chaitaliMohite@users.noreply.github.com>
2022-01-06 10:11:33 +0530

    initial commit

---
+++ DeltaCore/Areas/DeltaCoreProduct/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewModel;
+using BusinessRepository;
+
+namespace DeltaCore.Areas.DeltaCoreProduct.Controllers
+{
+    public class ProductsController : Controller
+    {
+        Repository repository = new Repository();
+        // GET: DeltaCoreProduct/Products
+        public ActionResult index()
+        {
+            return View();
+        }
+     
+        public JsonResult GetProduct()
+        {
+            List<VmProduct> vmproductlist = new List<VmProduct>();
+            try
+            {
+                vmproductlist = repository.GetProduct();
+            }
+            catch(Exception ex)
+            {
+                vmproductlist = new List<VmProduct>();
+            }
+            return Json(new { data = vmproductlist }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
